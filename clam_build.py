6e3742cec5c16c2c8f99efff9feed5dcb7e83f92
parumi <parumi@5e6ea8b2-dd2e-0410-9baf-e96cd7866457>
2006-03-21 19:27:53 +0000

    new clam build
    git-svn-id: https://testfarm.svn.sourceforge.net/svnroot/testfarm/trunk@32 5e6ea8b2-dd2e-0410-9baf-e96cd7866457

---
+++ clam_build.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+
+from testfarmclient import *
+
+clam = Repository("CLAM")
+clam.add_task("starting clam", ["echo foo"])
+
+'''
+clam.add_checking_for_new_commits( 
+	checking_cmd="cd $HOME/clam-sandboxes/testing-clam && cvs -nq up -dP | grep ^[UP]",  
+	minutes_idle=0.2
+)
+'''
+
+clam.add_deployment_task( [
+	"cd $HOME/clam-sandboxes",
+	"cvs co -d testing-clam CLAM",
+	"cvs co -d testing-smstools CLAM_SMSTools",
+	"cd $HOME/clam-sandboxes/testing-clam/scons/libs",
+	"scons configure prefix=/tmp",
+	"scons install",
+
+	"cd $HOME/clam-sandboxes",
+	"cvs co -d testing-smstools CLAM_SMSTools",
+	"cd testing-smstools/scons/QtSMSTools",
+	"scons clam_prefix=/tmp"
+] )
+clam.add_task("execute QTSMStools", [
+	"cd $HOME/clam-sandboxes/testing-smstools/scons/QtSMSTools",
+	"./QtSMSTools"
+] )
+
+
+TestFarmClient( "clam", [clam ], use_pushing_server=True, continuous=False )
+
