091cd1f724a46df47087180a44698203927f7ed4
parumi <parumi@5e6ea8b2-dd2e-0410-9baf-e96cd7866457>
2006-03-23 17:55:34 +0000

    better
    git-svn-id: https://testfarm.svn.sourceforge.net/svnroot/testfarm/trunk@44 5e6ea8b2-dd2e-0410-9baf-e96cd7866457

--- clam_build.py
+++ clam_build.py
@@ -4,22 +4,23 @@ from testfarmclient import *
 import os
 
 os.environ['LD_LIBRARY_PATH']='%s/clam-sandboxes/tlocal/lib:/usr/local/lib' % os.environ['PATH']
-
-
+def filter_cvs_update( text ):
+	dont_start_interr = lambda line : not line[0]=='?'
+	result = filter(dont_start_interr, text.split('\n') )	
+	return '\n'.join(result)
 clam = Repository("CLAM")
 clam.add_task("starting clam", ["echo foo"])
-'''
 clam.add_checking_for_new_commits( 
 	checking_cmd="cd $HOME/clam-sandboxes/testing-clam && cvs -nq up -dP | grep ^[UP]",  
-	minutes_idle=0.2
+	minutes_idle=5
 )
-'''
 clam.add_deployment_task( [
 	"cd $HOME/clam-sandboxes",
 #	"cvs co -d testing-clam CLAM",
-	"cd testing-clam && cvs up -dP",
+	{ CMD: "cd testing-clam && cvs -q up -dP", INFO: filter_cvs_update },
 	"cd $HOME/clam-sandboxes/testing-clam/scons/libs",
 	"scons configure prefix=$HOME/clam-sandboxes/tlocal",
+	"scons install",
 	"scons install",
 ] )
 
@@ -33,7 +34,7 @@ clam.add_task("SMSTools installation", [
 
 clam.add_task("execute QTSMStools", [
 	"cd $HOME/clam-sandboxes/testing-smstools/scons/QtSMSTools",
-	"./QtSMSTools"
+#	"./QtSMSTools"
 ] )
 
 clam.add_task("NetworkEditor installation", [
@@ -45,15 +46,18 @@ clam.add_task("NetworkEditor installation", [
 
 clam.add_task("execute NetworkEditor", [
 	"cd $HOME/clam-sandboxes/testing-neteditor/scons",
-	"./NetworkEditor"
+#	"./NetworkEditor"
 ] )
 
+
+shared_dir = "/iua-data/temp/users/parumi/clam_testsfarm_logs"
+local_dir = "%s/clam-sandboxes/testfarm_logs" % os.environ['HOME']
 
 TestFarmClient( 
 	"testing_machine_linux_breezy", 
 	[clam ], 
 	generated_html_path='./html-clam', 
-	logs_path='/home/testfarmclient/clam-sandboxes/testfarm_logs/',
+	logs_path=local_dir,
 	continuous=False 
 )
 
