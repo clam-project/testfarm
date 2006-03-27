f0a04ffed7c4759004b0f550d6ee375cd6150a54
xoliver <xoliver@5e6ea8b2-dd2e-0410-9baf-e96cd7866457>
2006-03-27 17:59:11 +0000

    small changes
    git-svn-id: https://testfarm.svn.sourceforge.net/svnroot/testfarm/trunk@68 5e6ea8b2-dd2e-0410-9baf-e96cd7866457

--- clam_build_osx.py
+++ clam_build_osx.py
@@ -3,7 +3,6 @@
 from testfarmclient import *
 import os
 
-#os.environ['LD_LIBRARY_PATH']='%s/clam-sandboxes/tlocal/lib:/usr/local/lib' % os.environ['PATH']
 def filter_cvs_update( text ):
 	dont_start_interr = lambda line : not line or not line[0]=='?'
 	result = filter(dont_start_interr, text.split('\n') )	
@@ -15,37 +14,39 @@ clam.add_checking_for_new_commits(
 	checking_cmd="cd $HOME/clam-sandboxes/testing-clam && cvs -nq up -dP | grep ^[UP]",  
 	minutes_idle=5
 )
+clam.add_task("Which new commits?", [	
+	"cd $HOME/clam-sandboxes",
+	{ CMD: "cd testing-clam && cvs -q up -dP", INFO: filter_cvs_update },
+] )
 clam.add_deployment_task( [
 	"cd $HOME/clam-sandboxes",
-	"cvs co -d testing-clam CLAM",
-#	"cd testing-clam && cvs up -dP",
-	{ CMD: "cd testing-clam && cvs -q up -dP", INFO: filter_cvs_update },
+#	"cvs co -d testing-clam CLAM",
+	"cd testing-clam && cvs up -dP",
 	"cd $HOME/clam-sandboxes/testing-clam/scons/libs",
 	"scons configure",
 	"scons",
-	"sudo scons install",
+#	"sudo scons install",
 ] )
 
 clam.add_task("SMSTools installation", [
 	"cd $HOME/clam-sandboxes",
-	"cvs co -d testing-smstools CLAM_SMSTools",
-#	"cd testing-smstools && cvs up -dP",
+#	"cvs co -d testing-smstools CLAM_SMSTools",
+	"cd testing-smstools && cvs up -dP",
 	"cd $HOME/clam-sandboxes/testing-smstools/scons/QtSMSTools",
-#	"scons clam_prefix=$HOME/clam-sandboxes/tlocal"
 	"scons"
 ] )
 
 clam.add_task("execute QTSMStools", [
 	"cd $HOME/clam-sandboxes/testing-smstools/scons/QtSMSTools",
+	"scons"
 #	"./QtSMSTools"
 ] )
 
 clam.add_task("NetworkEditor installation", [
 	"cd $HOME/clam-sandboxes",
-	"cvs co -d testing-neteditor CLAM_NetworkEditor",
-#	"cd testing-neteditor && cvs up -dP",
-	"cd testing-neteditor/scons",
-#	"scons clam_prefix=$HOME/clam-sandboxes/tlocal"
+#	"cvs co -d testing-neteditor CLAM_NetworkEditor",
+	"cd testing-neteditor && cvs up -dP",
+	"cd scons",
 	"scons"
 ] )
 
@@ -54,13 +55,9 @@ clam.add_task("execute NetworkEditor", [
 #	"./NetworkEditor"
 ] )
 
-local_dir = '%s/clam-sandboxes/testfarm_logs' % os.environ['HOME'] 
-
-shared_dir = "/iua-data/temp/users/parumi/clam_testsfarm_logs"
-local_dir = "%s/clam-sandboxes/testfarm_logs" % os.environ['HOME']
 
 TestFarmClient( 
-	"MacOSX_10.4.4", 
+	"testing-machine_osx-tiger", 
 	clam, 
 	remote_server_url='http://10.55.0.66/testfarm_server',
 	continuous=True
