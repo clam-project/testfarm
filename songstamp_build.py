e5f1e8ac905a996a0613de78aaf4c720f22fbe6b
parumi <parumi@5e6ea8b2-dd2e-0410-9baf-e96cd7866457>
2006-04-10 16:16:32 +0000

    uses remote server
    git-svn-id: https://testfarm.svn.sourceforge.net/svnroot/testfarm/trunk@96 5e6ea8b2-dd2e-0410-9baf-e96cd7866457

--- songstamp_build.py
+++ songstamp_build.py
@@ -10,13 +10,16 @@ def pass_text(text) :
 
 environ['SVN_SSH']='ssh -i %s/.ssh/svn_id_dsa' % environ['HOME']
 
-cd_songstamp = "cd $HOME/songstamp-sandboxes/clean-songstamp/"
-cd_songstamptest = "cd $HOME/songstamp-sandboxes/clean-songstamp/export/sdk-benchmark/example/"
+songstamp_path = "$HOME/songstamp-sandboxes/clean-songstamp/"
+cd_songstamp = "cd " + songstamp_path
+songstamptest_path = "$HOME/songstamp-sandboxes/clean-songstamp/export/sdk-benchmark/example/"
+cd_songstamptest = "cd " + songstamptest_path
+testdb_path = "$HOME/songstamp-sandboxes/test-db/"
 
 songstamp_update = 'svn update clean-songstamp'
 
 #songstampcheckout = 'svn checkout svn+ssh://testfarm@mtgdb.iua.upf.edu/fingerprint/songstamp/ clean-songstamp/'
-songstamp_checkout = 'svn checkout svn+ssh://svn@mtgdb.iua.upf.edu/fingerprint/songstamp/ clean-songstamp/'
+songstamp_checkout = 'svn checkout svn+ssh://testfarm@mtgdb.iua.upf.edu/fingerprint/songstamp/ clean-songstamp/'
 
 
 songstamp = Repository("songstamp")
@@ -33,6 +36,7 @@ songstamp.add_deployment_task([
 	"mkdir -p songstamp-sandboxes",
 	"cd songstamp-sandboxes",
 	"rm -rf clean-songstamp",
+#	songstamp_update
 	songstamp_checkout,
 #	{CMD : "svn diff --revision HEAD clean-songstamp/", INFO: pass_text},
 #	{CMD : songstamp_checkout, INFO : pass_text },
@@ -71,27 +75,22 @@ songstamp.add_task("build executables linked to the exported core library", [
 
 songstamp.add_task("SongStamp Extractor functional test ->  benchmark data", [
 	cd_songstamptest,
-	"dir=`dirname $0`",
-	#"export LD_LIBRARY_PATH=$dir/bin",
-	#"echo $LD_LIBRARY_PATH",
-	"$dir/bin/songstamp_extractor $dir/data/settings.bin $dir/audio/reference reference_audio.lst $dir/database interactive",
+	#"bin/songstamp_extractor data/settings.bin testdb_path/reference reference_audio.lst database interactive",
+	"bin/songstamp_extractor data/settings.bin " + testdb_path + "/reference reference_audio.lst database interactive",
 ] )
 
 
 songstamp.add_task("SongStamp Identifier functional test -> benchmark data", [
 	cd_songstamptest,
-	"dir=`dirname $0`",
-	#"export LD_LIBRARY_PATH=$dir/bin",
-	#"echo $LD_LIBRARY_PATH",
-	"$dir/bin/songstamp_identifier $dir/data/settings.bin $dir/database database_index.lst $dir/audio/user/dummy_22kHz_16bit_mono.wav $dir/playlist.lst",
+	"bin/songstamp_identifier data/settings.bin database database_index.lst testdb_path/user/dummy_22kHz_16bit_mono.wav playlist.lst",
 ] )
 
 
 TestFarmClient( 
-	'lars linux breezy', 
+	'testing-machine_linux_breezy', 
 	songstamp,  
-	html_base_dir='./html',
-	logs_base_dir='%s/songstamp-sandboxes/testfarm_logs' % environ['HOME'], #TODO can use $HOME ?
-#	remote_server_url='http://10.55.0.66/testfarm_server',
+#	html_base_dir='./html',
+#	logs_base_dir='%s/songstamp-sandboxes/testfarm_logs' % environ['HOME'], #TODO can use $HOME ?
+	remote_server_url='http://10.55.0.66/testfarm_server',
 	continuous=True 
 )
