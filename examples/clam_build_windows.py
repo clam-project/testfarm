d3240ae963fa7a9a48272671fe5f4fd84285dce5
parumi <parumi@5e6ea8b2-dd2e-0410-9baf-e96cd7866457>
2006-05-18 16:13:13 +0000

    updated
    git-svn-id: https://testfarm.svn.sourceforge.net/svnroot/testfarm/trunk@173 5e6ea8b2-dd2e-0410-9baf-e96cd7866457

--- examples/clam_build_windows.py
+++ examples/clam_build_windows.py
@@ -2,16 +2,25 @@
 
 import sys
 sys.path.append('../src')
-from os import environ
-from testfarmclient import *
+from task import *
+from project import Project
+from client import Client
+from runner import Runner
+import os, time
 
-def pass_text(text) :
-	return text
+start_time = -1
+def start_timer(output):
+	global start_time
+	start_time = time.time()
+def exectime_unittests(output):
+	global start_time
+	return {'exectime_unittests' : time.time() - start_time}
+def exectime_functests(output):
+	global start_time
+	return {'exectime_functests' : time.time() - start_time}
 
 def force_ok(text):
 	return True
-
-
 def filter_cvs_update( text ):
 	dont_start_interr = lambda line : not line or not line[0]=='?'
 	result = filter(dont_start_interr, text.split('\n') )	
@@ -22,14 +31,19 @@ def set_qtdir_to_qt3(x) : environ['QTDIR']='f:\\clam-external-libs\\qt3\\'
 
 cd_clam = 'cd f:\\clam-sandboxes'
 
-clam = Repository("CLAM")
+clam = Task(
+	project = Project("CLAM"), 
+	client = Client("testing_machine-windows"), 
+	task_name="with cvs update" 
+	)
+
 
 clam.add_checking_for_new_commits( 
 	checking_cmd='cd f:\\clam-sandboxes\\ && cvs -nq up -dP testing-clam testing-vmqt testing-annotator testing-neteditor testing-smstools | grep ^[UP]',  
 	minutes_idle=5
 )
 
-clam.add_deployment_task([
+clam.add_deployment([
 	cd_clam,
 	"cd testing-clam",
 	{ CMD: "cvs -q up -dP", INFO: filter_cvs_update },
@@ -39,10 +53,10 @@ clam.add_deployment_task([
 'scons configure prefix=f:\\clam-sandboxes\\local sandbox_path=f:\\clam-external-libs  qt_includes=f:\\clam-external-libs\\qt\\include qt_libs=f:\\clam-external-libs\\qt\\lib release=1 double=1',
 	'scons',
 	'scons install',
-	'scons install', #TODO bug? check if repeating scons install is really necessary.
+#	'scons install', #TODO bug? check if repeating scons install is really necessary.
 ] )
 
-clam.add_task('unit tests', [
+clam.add_subtask('unit tests', [
 	cd_clam,
 	'cd testing-clam\\scons\\tests',
 	'scons clam_prefix=f:\\clam-sandboxes\\local install_prefix=f:\\clam-sandboxes\\local cppunit_prefix=f:\\clam-external-libs\cppunit test_data_path=f:\\clam-sandboxes\\CLAM-TestData release=1 double=1 unit_tests',
@@ -50,7 +64,7 @@ clam.add_task('unit tests', [
 	{CMD: 'UnitTests.exe', INFO: pass_text, STATUS_OK: force_ok },
 	])
 
-clam.add_task('functional test', [
+clam.add_subtask('functional test', [
 	cd_clam,
 	'cd testing-clam\\scons\\tests',
 	'scons clam_prefix=f:\\clam-sandboxes\\local install_prefix=f:\\clam-sandboxes\\local cppunit_prefix=f:\\clam-external-libs\cppunit test_data_path=f:\\clam-sandboxes\\CLAM-TestData release=1 double=1 functional_tests',
@@ -58,21 +72,21 @@ clam.add_task('functional test', [
 	{CMD: 'FunctionalTests.exe', INFO: pass_text, STATUS_OK: force_ok },
 	])
 
-clam.add_task('clam examples compilation', [
+clam.add_subtask('clam examples compilation', [
 	cd_clam,
 	'cd testing-clam\\scons\\examples',
 	'scons clam_prefix=f:\\clam-sandboxes\\local release=1 double=1',
 	])
 
 
-clam.add_task('smstools compilation', [
+clam.add_subtask('smstools compilation', [
 	cd_clam,
 	'cd testing-smstools',
 	{ CMD: "cvs -q up -dP", INFO: filter_cvs_update },
 	'cd scons\\QtSMSTools',
 	'scons clam_prefix=f:\\clam-sandboxes\\local install_prefix=\\clam-sandboxes\\local release=1 double=1'
 ] )
-clam.add_task('network editor compilation', [
+clam.add_subtask('network editor compilation', [
 	cd_clam,
 	'cd testing-neteditor',
 	{ CMD: "cvs -q up -dP", INFO: filter_cvs_update },
@@ -80,7 +94,7 @@ clam.add_task('network editor compilation', [
 	'scons clam_prefix=f:\\clam-sandboxes\\local install_prefix=\\clam-sandboxes\\local release=1 double=1'
 ] )
 
-clam.add_task('vmqt compilation and examples', [
+clam.add_subtask('vmqt compilation and examples', [
 	cd_clam,
 	'cd testing-vmqt',
 	{ CMD: "cvs -q up -dP", INFO: filter_cvs_update },
@@ -90,7 +104,7 @@ clam.add_task('vmqt compilation and examples', [
 ] )
 
 
-clam.add_task('annotator compilation', [
+clam.add_subtask('annotator compilation', [
 	cd_clam,
 	'cd testing-annotator',
 	{ CMD: "cvs -q up -dP", INFO: filter_cvs_update },
@@ -100,11 +114,7 @@ clam.add_task('annotator compilation', [
 
 
 
-TestFarmClient( 
-	'testing-machine_windows', 
+Runner( 
 	clam,  
-	#generated_html_path='./html',
-	#logs_path='g:\\sandbox\\clam-sandbox\\testfarm_logs',
 	remote_server_url="http://10.55.0.66/testfarm_server",
-	continuous=True
 )
