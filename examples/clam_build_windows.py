a905220c86cee18c72a9b9e96811a79b5ba42a33
testfarm <testfarm@5e6ea8b2-dd2e-0410-9baf-e96cd7866457>
2006-05-30 09:08:51 +0000

    tlocal instead of local
    git-svn-id: https://testfarm.svn.sourceforge.net/svnroot/testfarm/trunk@202 5e6ea8b2-dd2e-0410-9baf-e96cd7866457

--- examples/clam_build_windows.py
+++ examples/clam_build_windows.py
@@ -61,7 +61,7 @@ clam.add_deployment([
 	cd_clam,
 	'cd testing-clam\\scons\\libs',
 	{CMD: "echo seting QTDIR to qt3 path ", INFO: set_qtdir_to_qt3},
-'scons configure prefix=f:\\clam-sandboxes\\local sandbox_path=f:\\clam-external-libs  qt_includes=f:\\clam-external-libs\\qt\\include qt_libs=f:\\clam-external-libs\\qt\\lib with_portmidi=1 release=1 double=1',
+'scons configure prefix=f:\\clam-sandboxes\\tlocal sandbox_path=f:\\clam-external-libs  qt_includes=f:\\clam-external-libs\\qt\\include qt_libs=f:\\clam-external-libs\\qt\\lib with_portmidi=1 release=1 double=1',
 	'scons',
 	'scons install',
 #	'scons install', #TODO bug? check if repeating scons install is really necessary.
@@ -71,7 +71,7 @@ clam.add_subtask('unit tests', [
 	cd_clam,
 	'cd testing-clam\\scons\\tests',
 	{CMD: "echo seting QTDIR to qt3 path ", INFO: set_qtdir_to_qt3},
-	'scons clam_prefix=f:\\clam-sandboxes\\local install_prefix=f:\\clam-sandboxes\\local cppunit_prefix=f:\\clam-external-libs\cppunit test_data_path=f:\\clam-sandboxes\\CLAM-TestData release=1 double=1 unit_tests',
+	'scons clam_prefix=f:\\clam-sandboxes\\tlocal install_prefix=f:\\clam-sandboxes\\tlocal cppunit_prefix=f:\\clam-external-libs\cppunit test_data_path=f:\\clam-sandboxes\\CLAM-TestData release=1 double=1 unit_tests',
 	'cd unit_tests',
 	{CMD: 'UnitTests.exe', INFO: lambda x:x, STATUS_OK: force_ok },
 	])
@@ -80,7 +80,7 @@ clam.add_subtask('functional test', [
 	cd_clam,
 	'cd testing-clam\\scons\\tests',
 	{CMD: "echo seting QTDIR to qt3 path ", INFO: set_qtdir_to_qt3},
-	'scons clam_prefix=f:\\clam-sandboxes\\local install_prefix=f:\\clam-sandboxes\\local cppunit_prefix=f:\\clam-external-libs\cppunit test_data_path=f:\\clam-sandboxes\\CLAM-TestData release=1 double=1 functional_tests',
+	'scons clam_prefix=f:\\clam-sandboxes\\tlocal install_prefix=f:\\clam-sandboxes\\tlocal cppunit_prefix=f:\\clam-external-libs\cppunit test_data_path=f:\\clam-sandboxes\\CLAM-TestData release=1 double=1 functional_tests',
 	'cd functional_tests',
 	{CMD: 'FunctionalTests.exe', INFO: lambda x:x, STATUS_OK: force_ok },
 	])
@@ -89,7 +89,7 @@ clam.add_subtask('clam examples compilation', [
 	cd_clam,
 	'cd testing-clam\\scons\\examples',
 	{CMD: "echo seting QTDIR to qt3 path ", INFO: set_qtdir_to_qt3},
-	'scons clam_prefix=f:\\clam-sandboxes\\local release=1 double=1',
+	'scons clam_prefix=f:\\clam-sandboxes\\tlocal release=1 double=1',
 	])
 
 
@@ -98,14 +98,14 @@ clam.add_subtask('smstools compilation', [
 	'cd testing-smstools',
 	{ CMD: "cvs -q up -dP", INFO: filter_cvs_update },
 	{CMD: "echo seting QTDIR to qt3 path ", INFO: set_qtdir_to_qt3},
-	'scons clam_prefix=f:\\clam-sandboxes\\local install_prefix=\\clam-sandboxes\\local clam_sconstools=f:\\clam-sandboxes\\testing-clam\\scons\\sconstools release=1 double=1'
+	'scons clam_prefix=f:\\clam-sandboxes\\tlocal install_prefix=\\clam-sandboxes\\tlocal clam_sconstools=f:\\clam-sandboxes\\testing-clam\\scons\\sconstools release=1 double=1'
 ] )
 clam.add_subtask('network editor compilation', [
 	cd_clam,
 	'cd testing-neteditor',
 	{ CMD: "cvs -q up -dP", INFO: filter_cvs_update },
 	{CMD: "echo seting QTDIR to qt3 path ", INFO: set_qtdir_to_qt3},
-	'scons clam_prefix=f:\\clam-sandboxes\\local install_prefix=\\clam-sandboxes\\local clam_sconstools=f:\\clam-sandboxes\\testing-clam\\scons\\sconstools release=1 double=1'
+	'scons clam_prefix=f:\\clam-sandboxes\\tlocal install_prefix=\\clam-sandboxes\\tlocal clam_sconstools=f:\\clam-sandboxes\\testing-clam\\scons\\sconstools release=1 double=1'
 ] )
 
 clam.add_subtask('vmqt compilation and examples', [
@@ -113,7 +113,7 @@ clam.add_subtask('vmqt compilation and examples', [
 	'cd testing-vmqt',
 	{ CMD: "cvs -q up -dP", INFO: filter_cvs_update },
 	{CMD: "echo seting QTDIR to qt4 path ", INFO: set_qtdir_to_qt4},
-	'scons clam_prefix=f:\\clam-sandboxes\\local install_prefix=\\clam-sandboxes\\local clam_sconstools=f:\\clam-sandboxes\\testing-clam\\scons\\sconstools release=1 double=1'
+	'scons clam_prefix=f:\\clam-sandboxes\\tlocal install_prefix=\\clam-sandboxes\\tlocal clam_sconstools=f:\\clam-sandboxes\\testing-clam\\scons\\sconstools release=1 double=1'
 	'scons examples',
 ] )
 
@@ -123,7 +123,7 @@ clam.add_subtask('annotator compilation', [
 	'cd testing-annotator',
 	{ CMD: "cvs -q up -dP", INFO: filter_cvs_update },
 	{CMD: "echo seting QTDIR to qt4 path ", INFO: set_qtdir_to_qt4},
-	'scons clam_prefix=f:\\clam-sandboxes\\local install_prefix=\\clam-sandboxes\\local clam_sconstools=f:\\clam-sandboxes\\testing-clam\\scons\\sconstools clam_vmqt4_path=f:\\clam-sandboxes\\testing-vmqt release=1 double=1'
+	'scons clam_prefix=f:\\clam-sandboxes\\tlocal install_prefix=\\clam-sandboxes\\tlocal clam_sconstools=f:\\clam-sandboxes\\testing-clam\\scons\\sconstools clam_vmqt4_path=f:\\clam-sandboxes\\testing-vmqt release=1 double=1'
 ] )
 
 
