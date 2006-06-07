ebda27aab7350be57acfe07c2e80783711c9d61f
lfabig <lfabig@5e6ea8b2-dd2e-0410-9baf-e96cd7866457>
2006-06-07 15:17:56 +0000

    updated
    git-svn-id: https://testfarm.svn.sourceforge.net/svnroot/testfarm/trunk@226 5e6ea8b2-dd2e-0410-9baf-e96cd7866457

--- examples/songstamp_build_windows.py
+++ examples/songstamp_build_windows.py
@@ -46,6 +46,7 @@ cd_songstamptest = "cd " + songstamptest_path
 
 testdb_path = root_path + "\\test-db"
 
+#fingerprint_path = root_path + "\\local\\database"
 fingerprint_path = root_path + "\\local\\database"
 cd_fingerprint_path = "cd " + fingerprint_path
 
@@ -68,88 +69,91 @@ songstamp.set_check_for_new_commits(
 )
 
 songstamp.add_deployment([
-	#cd_root_path,
 	{CMD : "cd d:\\sandbox\\fingerprint-sandboxes", INFO : pass_text },
 	{CMD : "mkdir clean-fingerprint", STATUS_OK : force_ok},	
 	"cd clean-fingerprint",
 	{CMD : songstamp_update, INFO : pass_text },
-#	songstamp_update,
 #	{CMD : songstamp_checkout, INFO : pass_text },
-#	songstamp_checkout,
 #	{CMD : "svn diff --revision HEAD clean-songstamp/", INFO: pass_text},
-#	{CMD : songstamp_checkout, INFO : pass_text },
 ] )
 
-#songstamp.add_subtask("make install directory", [
-#	cd_root_path,
-#	"mkdir -p songstamp-app-install",
-#] )
+songstamp.add_subtask("make install directory", [
+	cd_root_path,
+	{CMD : "mkdir songstamp-app-install", STATUS_OK : force_ok},
+] )
 
-#songstamp.add_subtask("build SongStamp core library", [
-#	cd_songstamp_library,
-##	"scons benchmark=1 prefix=" + install_path,
-#	"scons benchmark=1 prefix=" + root_path + "\\songstamp-app-install",
-#] )	
+songstamp.add_subtask("build SongStamp core library", [
+	cd_songstamp_library,
+	{CMD : "del options.cache", INFO : pass_text },
+	"scons benchmark=1 prefix=" + root_path + "\\songstamp-app-install",
+] )	
 	
-#songstamp.add_subtask("build SongStamp application", [
-#	cd_songstamp_app,
-##	"scons benchmark=1 prefix=" + install_path,	
-#	"scons benchmark=1 prefix=" + root_path + "\\songstamp-app-install",	
-#] )
+songstamp.add_subtask("build SongStamp application", [
+	cd_songstamp_app,
+	{CMD : "del options.cache", INFO : pass_text },	
+	"scons benchmark=1 prefix=" + root_path + "\\songstamp-app-install",	
+] )
 
-#songstamp.add_subtask("copy data files to install path", [
-##	cd_install_path,
-#	cd_root_path,
-#	"cd songstamp-app-install",
-#	"mkdir -p data",
-#	"mkdir -p database",	
-#	cd_root_path,
-##	{CMD:"pwd && cd clean-fingerprint/", INFO: pass_text },
-#	"cp clean-fingerprint/data/aida4-models/model.bin local/data/",
-#] )
+songstamp.add_subtask("copy data files to install path", [
+	cd_root_path,
+	{CMD : "songstamp-app-install", STATUS_OK : force_ok},
+	"cd songstamp-app-install",
+	{CMD : "mkdir data", STATUS_OK : force_ok},
+	{CMD : "mkdir database", STATUS_OK : force_ok},	
+	cd_root_path,
+	{CMD:"copy clean-fingerprint\\data\\aida4-models\\model.bin songstamp-app-install\\data", INFO: pass_text},
+] )
 
 
-#songstamp.add_subtask("clean-up database", [
-#	cd_fingerprint_path,
-#	"rm -f *.afp",
-#	"rm -f *.lst",
-#] )
+songstamp.add_subtask("clean-up database", [
+	{CMD : "cd d:\\sandbox\\fingerprint-sandboxes\songstamp-app-install", INFO : pass_text },	
+	{CMD:"del /F *.afp", INFO: pass_text},	
+	{CMD:"del /F *.lst", INFO: pass_text},		
+] )
 
 # Test: SongStampApp
 
-#songstamp.add_subtask("SongStamp Extractor functional test ->  benchmark data", [
+songstamp.add_subtask("SongStamp Extractor functional test ->  benchmark data", [
+	{CMD : "cd d:\\sandbox\\fingerprint-sandboxes\\songstamp-app-install", INFO : pass_text },	
 #	cd_songstamptest,
-#	"bin/songstamp_app_extractor_benchmark data/model.bin " + testdb_path + "reference reference_audio_simple.lst database rebuild",
-#] )
+	{CMD : "bin\\songstamp_app_extractor_benchmark data\\model.bin " + testdb_path + "\\reference reference_audio_simple.lst database rebuild", INFO : pass_text },
+] )
 
 
-#songstamp.add_subtask("SongStamp Identifier functional test -> benchmark data", [
-#	cd_songstamptest,
-#	"bin/songstamp_app_identifier_benchmark data/model.bin database database_index.lst " + testdb_path + "user/dummy_22kHz_16bit_mono_simple.wav playlist.lst",
-#] )
+songstamp.add_subtask("SongStamp Identifier functional test -> benchmark data", [
+	{CMD : "cd d:\\sandbox\\fingerprint-sandboxes\\songstamp-app-install", INFO : pass_text },	
+#cd_songstamptest,
+	{CMD : "bin\\songstamp_app_identifier_benchmark data\\model.bin database database_index.lst " + testdb_path + "\\user\\dummy_22kHz_16bit_mono_simple.wav playlist.lst", INFO : pass_text },
+] )
 
 # Test: SongStampDaemon
 
+#TODO: install ACE
 #songstamp.add_subtask("make subdir for songstamp daemon install", [
 #	cd_root_path,
-#	"mkdir -p songstamp-daemon-install",
+#	{CMD : "mkdir songstamp-daemon-install", STATUS_OK : force_ok},	
 #] )
 
 #songstamp.add_subtask("build SongStamp core library for mobile gsm", [
 #	cd_songstamp_library,
+#	{CMD : "del options.cache", INFO : pass_text },	
 #	"scons mobile_gsm=1 prefix=" + root_path + "\\songstamp-daemon-install",
 #] )	
 
 #songstamp.add_subtask("build SongStamp daemon", [
-#	"cd " + root_path +"\\clean-fingerprint/songstamp_daemon/trunk/",
+#	"cd " + root_path +"\\clean-fingerprint\\songstamp_daemon\\trunk",
+#	{CMD : "del options.cache", INFO : pass_text },		
 #	"scons mobile_gsm=1 prefix=" + root_path + "\\songstamp-daemon-install",	
 #] )
 
 # Test: SongStamp Training
 
+# TODO: Install Essentia
+
 #songstamp.add_subtask("make subdir for songstamp training install", [
 #	cd_root_path,
-#	"mkdir -p songstamp-training-install",
+#	{CMD : "mkdir songstamp-training-install", STATUS_OK : force_ok},		
+##	"mkdir -p songstamp-training-install",
 #] )
 
 #songstamp.add_subtask("build SongStamp core library for training", [
@@ -158,7 +162,7 @@ songstamp.add_deployment([
 #] )
 
 #songstamp.add_subtask("build SongStamp Training", [
-#	"cd " + root_path + "clean-fingerprint/songstamp_training/trunk/",
+#	"cd " + root_path + "clean-fingerprint\\songstamp_training\\trunk",
 #	"scons training=1 prefix=" + root_path + "\\songstamp-training-install",
 #] )
 
