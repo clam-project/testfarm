e47ba81931d4962e0f4c8f7a0efddf0416e39efb
parumi <parumi@5e6ea8b2-dd2e-0410-9baf-e96cd7866457>
2007-04-26 08:05:06 +0000

    * service_proxy.py: do not use upf proxy for clam testfarm server (probably upf proxy should not be set by default)
    git-svn-id: https://testfarm.svn.sourceforge.net/svnroot/testfarm/trunk@303 5e6ea8b2-dd2e-0410-9baf-e96cd7866457

--- src/service_proxy.py
+++ src/service_proxy.py
@@ -22,8 +22,8 @@ import urllib
 import urlparse
 
 Proxies = { 'http' : 'http://proxy.upf.edu:8080', 'ftp' : 'http://proxy.upf.edu:8080' }
-NoProxiesFor = ["localhost", "127.0.0.1", "10.55.0.50", "10.55.0.66"]
-useragent = 'simac-annotator-tasker'
+NoProxiesFor = ["localhost", "127.0.0.1", "10.55.0.50", "10.55.0.66", "efpc072.upf.es"]
+useragent = 'testfarm-python-client'
 
 
 class ServiceProxy :
