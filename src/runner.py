5acb12b9d43f1618066867a2eababed605a02b61
vokimon <vokimon@5e6ea8b2-dd2e-0410-9baf-e96cd7866457>
2012-05-17 17:31:53 +0000

    space clearance
    git-svn-id: https://testfarm.svn.sourceforge.net/svnroot/testfarm/trunk@392 5e6ea8b2-dd2e-0410-9baf-e96cd7866457

--- src/runner.py
+++ src/runner.py
@@ -1,7 +1,7 @@
 #
-#  Copyright (c) 2006 Pau Arumi, Bram de Jong, Mohamed Sordo 
+#  Copyright (c) 2006 Pau Arumi, Bram de Jong, Mohamed Sordo
 #  and Universitat Pompeu Fabra
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -22,43 +22,43 @@
 from task import *
 from webgenerator import WebGenerator
 from listeners import ConsoleResultListener, MultiListener
-from serverlistener import ServerListener 
+from serverlistener import ServerListener
 from serverlistenerproxy import ServerListenerProxy
 
 class Runner :
 	"The interface to another modules. It runs a defined script and sends information to listeners."
-	def __init__(self, 
+	def __init__(self,
 		task,
 		continuous=False,
 		first_run_always=True,
-		local_base_dir = None,	
+		local_base_dir = None,
 		remote_server_url = None,
 		verbose = False,
 		testinglisteners = [],
 		extra_listeners = [],
 	) :
-		"Runs a task defined in user's script"	
+		"Runs a task defined in user's script"
 		self.listeners = [ ConsoleResultListener() ]
 
 		serverlistener = None # for keyboard interrupt purpose
 
 		if remote_server_url:
 			listenerproxy = ServerListenerProxy(
-				client=task.client, 
+				client=task.client,
 				service_url=remote_server_url,
 				project=task.project
-			)		
+			)
 			self.listeners.append( listenerproxy )
-		if local_base_dir :	
-			serverlistener = ServerListener( 
-				client=task.client, 
+		if local_base_dir :
+			serverlistener = ServerListener(
+				client=task.client,
 				logs_base_dir=local_base_dir + "/logs",
 				project=task.project
 			)
 			self.listeners.append( serverlistener )
-			server_to_push = WebGenerator( 
-				logs_base_dir=local_base_dir + "/logs", 
-				html_base_dir=local_base_dir + "/html", 
+			server_to_push = WebGenerator(
+				logs_base_dir=local_base_dir + "/logs",
+				html_base_dir=local_base_dir + "/html",
 				project_name=task.project.name)
 
 		else:
@@ -70,7 +70,7 @@ class Runner :
 		self.listeners += extra_listeners
 
 		try :
-			#do_subtasks at least one time	
+			#do_subtasks at least one time
 			new_commits_found = task.do_checking_for_new_commits( self.listeners, verbose=verbose ) #this creates a valid .idle file
 			print "first_run_always ",first_run_always
 			print "new commits_found ", new_commits_found
