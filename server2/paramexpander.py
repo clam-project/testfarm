166c139c1c28828b1f140a4bda6a328406a40a8f
David García Garzón <voki@canvoki.net>
2013-04-05 13:45:09 +0200

    paramexpander: shared optional and keyword args

--- server2/paramexpander.py
+++ server2/paramexpander.py
@@ -7,32 +7,54 @@ def expandfunction(expander) :
 		fargs, fvarargs, fkeywords, fdefaults = inspect.getargspec(f)
 		gdefaultvars = gargs[-len(gdefaults):] if gdefaults else []
 		fdefaultvars = fargs[-len(fdefaults):] if fdefaults else []
+		defaultvars = fdefaultvars + gdefaultvars
+		keywords = fkeywords or gkeywords
 		wrapperSpec = inspect.formatargspec(
 			[arg for arg in fargs
-				if arg not in fdefaultvars
+				if arg not in defaultvars
 			]+
 			[arg for arg in gargs
 				if  arg not in fargs
 				and arg not in gdefaultvars
 			]+
 			([arg for arg in fdefaultvars])+
-			([arg for arg in gdefaultvars]),
+			([arg for arg in gdefaultvars
+				if arg not in fdefaultvars]),
 			None,
-			None,
-			(fdefaults or ()) + (gdefaults or ())
+			keywords,
+			list(fdefaults or []) + [
+			value
+			for var, value in zip(gdefaultvars, gdefaults or [])
+			if var not in fdefaultvars]
 			)
-		gcall = ",".join((
-			"{var}={var}".format(var=var)
-			for var in gargs
-			))
+		gcall = [
+				"{var}={var}".format(var=var)
+				for var in gargs
+			]
+		if gkeywords :
+			gcall += [
+				"{var}={var}".format(var=var)
+				for var in fargs
+				if arg not in gargs
+			]+["**{}".format(keywords)]
+		fcall = [
+				"{var}={var}".format(var=var)
+				for var in fargs
+			]
+		if fkeywords :
+			fcall += [
+				"{var}={var}".format(var=var)
+				for var in gargs
+				if var not in fargs
+			] + ["**{}".format(keywords) ]
 		wrapperSource = """\
 def wrapper {wrapperSpec} :
 	g ({gcall})
 	return f ({fcall})
 """.format(
 	wrapperSpec = wrapperSpec,
-	gcall = gcall,
-	fcall = "a,b",
+	gcall = ",".join(gcall),
+	fcall = ",".join(fcall),
 	)
 		exec wrapperSource in locals(), globals()
 		wrapper.__name__ = f.__name__
--- server2/paramexpander_test.py
+++ server2/paramexpander_test.py
@@ -92,7 +92,7 @@ class ParamExpanderTest(unittest.TestCase) :
 		self.assertEqual(adaptee(1,2,3), (1,2))
 		self.assertEqual(expanderReturns, [3])
 
-	def test_expandfunction_withCommonArgs(self) :
+	def test_expandfunction_sharedArgs(self) :
 		expanderReturns = []
 		def expander(b): expanderReturns.append(b)
 		@expandfunction(expander)
@@ -133,7 +133,127 @@ class ParamExpanderTest(unittest.TestCase) :
 		self.assertEqual(expanderReturns, [(2,4)])
 
 
+	def test_expandfuction_sharedOptionalInAdaptee(self) :
+		expanderReturns = []
+		def expander(m): expanderReturns.append(m)
+		@expandfunction(expander)
+		def adaptee(a, m=66): return a,m
 
+		self.assertSignatureEqual(adaptee, "(a, m=66)")
+		self.assertEqual(adaptee(1,2), (1,2))
+		self.assertEqual(expanderReturns, [2])
+
+	def test_expandfuction_sharedOptionalInExpander(self) :
+		expanderReturns = []
+		def expander(m=66): expanderReturns.append(m)
+		@expandfunction(expander)
+		def adaptee(a, m): return a,m
+
+		self.assertSignatureEqual(adaptee, "(a, m=66)")
+		self.assertEqual(adaptee(1,2), (1,2))
+		self.assertEqual(expanderReturns, [2])
+
+	def test_expandfuction_sharedOptionalInBoth(self) :
+		# TOREVIEW: No reason for taking adaptee over expander default
+		expanderReturns = []
+		def expander(m=99): expanderReturns.append(m)
+		@expandfunction(expander)
+		def adaptee(a, m=66): return a,m
+
+		self.assertSignatureEqual(adaptee, "(a, m=66)")
+		self.assertEqual(adaptee(1), (1,66))
+		self.assertEqual(expanderReturns, [66])
+
+	def test_expandfuction_sharedOptionalInBoth(self) :
+		# TOREVIEW: No reason for taking adaptee over expander default
+		expanderReturns = []
+		def expander(m=99): expanderReturns.append(m)
+		@expandfunction(expander)
+		def adaptee(a, m=66): return a,m
+
+		self.assertSignatureEqual(adaptee, "(a, m=66)")
+		self.assertEqual(adaptee(1), (1,66))
+		self.assertEqual(expanderReturns, [66])
+
+	def test_expandfuction_keywordCallAllowDisorder(self) :
+		expanderReturns = []
+		def expander(x,m,y): expanderReturns.append((x,m,y))
+		@expandfunction(expander)
+		def adaptee(m,a,b,c): return m,a,b,c
+
+		self.assertSignatureEqual(adaptee, "(m, a, b, c, x, y)")
+		self.assertEqual(adaptee(x='x',a='a',c='c',b='b',y='y',m='m'), ('m','a','b','c'))
+		self.assertEqual(expanderReturns, [('x','m','y')])
+
+	def test_expandFunction_expanderKwdsTakesAdapteeArgs(self) :
+		expanderReturns = []
+		def expander(**kwds): expanderReturns.append(kwds)
+		@expandfunction(expander)
+		def adaptee(a): return a
+
+		self.assertSignatureEqual(adaptee, "(a, **kwds)")
+		self.assertEqual(adaptee(1), 1)
+		self.assertEqual(expanderReturns, [dict(a=1)])
+
+	def test_expandFunction_expanderKwdsTakesExtraArgs(self) :
+		expanderReturns = []
+		def expander(**kwds): expanderReturns.append(kwds)
+		@expandfunction(expander)
+		def adaptee(): return "boo"
+
+		self.assertSignatureEqual(adaptee, "(**kwds)")
+		self.assertEqual(adaptee(j=3), "boo")
+		self.assertEqual(expanderReturns, [dict(j=3)])
+
+	def test_expandFunction_expanderKwdsRemovesExplicitArgs(self) :
+		expanderReturns = []
+		def expander(m, **kwds): expanderReturns.append((m,kwds))
+		@expandfunction(expander)
+		def adaptee(): return "boo"
+
+		self.assertSignatureEqual(adaptee, "(m, **kwds)")
+		self.assertEqual(adaptee(1,j=3), "boo")
+		self.assertEqual(expanderReturns, [(1,dict(j=3))])
+
+	def test_expandFunction_expanderKwdsSharedKeys(self) :
+		expanderReturns = []
+		def expander(m, **kwds): expanderReturns.append((m,kwds))
+		@expandfunction(expander)
+		def adaptee(m): return m
+
+		self.assertSignatureEqual(adaptee, "(m, **kwds)")
+		self.assertEqual(adaptee(1,j=3), 1)
+		self.assertEqual(expanderReturns, [(1,dict(j=3))])
+
+	def test_expandFunction_adapteeKwdsTakesExpanderArgs(self) :
+		expanderReturns = []
+		def expander(a): expanderReturns.append(a)
+		@expandfunction(expander)
+		def adaptee(**kwds): return kwds
+
+		self.assertSignatureEqual(adaptee, "(a, **kwds)")
+		self.assertEqual(adaptee(1), dict(a=1))
+		self.assertEqual(expanderReturns, [1])
+
+	def test_expandFunction_adapteeKwdsTakesExtraArgs(self) :
+		expanderReturns = []
+		def expander(): expanderReturns.append("boo")
+		@expandfunction(expander)
+		def adaptee(**kwds): return kwds
+
+		self.assertSignatureEqual(adaptee, "(**kwds)")
+		self.assertEqual(adaptee(j=3), dict(j=3))
+		self.assertEqual(expanderReturns, ["boo"])
+
+	def test_expandFunction_adapteeKwdsTakesExtraArgs(self) :
+		expanderReturns = []
+		def expander(m): expanderReturns.append(m)
+		@expandfunction(expander)
+		def adaptee(m,**kwds): return m,kwds
+
+		self.assertSignatureEqual(adaptee, "(m, **kwds)")
+		self.assertEqual(adaptee(1,j=3), (1,dict(j=3)))
+		self.assertEqual(expanderReturns, [1])
 
 
 
@@ -142,12 +262,12 @@ TODO:
 + adaptee name remains
 + adaptee doc remains
 + optionals in expander
-- optionals in adaptee
-- optionals in both sorted adaptee, then expander
-- optional in expander remains optional
-- optional in adaptee remains optional
-- optional in both, takes adaptee value (yes?)
-- keyword call allows disordering
++ optionals in adaptee
++ optionals in both sorted adaptee, then expander
++ shared optional in expander remains optional
++ shared optional in adaptee remains optional
++ shared optional in both, takes adaptee value (yes?)
++ keyword call allows disordering
 - varargs, what to do with them?
 - keywordargs, what to do with them?
 """
