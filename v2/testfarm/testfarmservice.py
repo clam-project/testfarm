09f3525706829dbad9a42b7189906961835d2969
David García Garzón <voki@canvoki.net>
2013-05-02 15:24:00 +0200

    Privatized service data

--- v2/testfarm/testfarmservice.py
+++ v2/testfarm/testfarmservice.py
@@ -1,31 +1,32 @@
-from server import Server
-path = "projects"
-s = Server(path)
+from testfarm.server import Server as _Server
+_path = "projects"
+_s = _Server(_path)
+
+api = "2.0"
 
 def clientIdle(project, client, minutes) :
-	import datetime
-	s.clientIdle(project, client, int(minutes))
+	_s.clientIdle(project, client, int(minutes))
 
 def executionStarts( project, client, execution) :
-	s.executionStars(project, client, execution)
+	_s.executionStars(project, client, execution)
 
 def taskStarts( project, client, execution, task, description) :
-	s.taskStarts(project, client, execution, task, description)
+	_s.taskStarts(project, client, execution, task, description)
 
 def commandStarts( project, client, execution, task, command, commandline) :
-	s.commandStarts( project, client, execution, task, command, commandline)
+	_s.commandStarts( project, client, execution, task, command, commandline)
 
 def commandEnds(project, client, execution,
 		task, command, output, ok, info, stats
 		) :
-	s.commandEnds(project, client, execution,
+	_s.commandEnds(project, client, execution,
 		task, command, output, ok, info, stats
 		)
 
 def taskEnds( project, client, execution, task, ok) :
-	s.taskEnds( project, client, execution, task, ok)
+	_s.taskEnds( project, client, execution, task, ok)
 
 def executionEnds( project, client, execution, ok) :
-	s.executionEnds( project, client, execution, ok)
+	_s.executionEnds( project, client, execution, ok)
 
 
