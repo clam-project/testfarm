a84bb2bda60e487827c2f7c355c98eac7707b6b0
David García Garzón <voki@canvoki.net>
2013-05-03 16:39:19 +0200

    use ast to parse execstarts extras

--- v2/testfarm/testfarmservice.py
+++ v2/testfarm/testfarmservice.py
@@ -1,5 +1,6 @@
 from testfarm.server import Server as _Server
 import os as _os
+import ast as _ast
 def _server(request) :
 	print request.environ.keys()
 	_path = request.environ.get("TESTFARM_LOGPATH", "projects")
@@ -14,6 +15,7 @@ def clientIdle(request, project, client, minutes) :
 
 def executionStarts(request, project, client, execution, **kwds) :
 	s = _server(request)
+	kwds = { k:_ast.literal_eval(v) for k,v in kwds }
 	s.executionStarts(project, client, execution, **kwds)
 
 def taskStarts(request, project, client, execution, task, description) :
@@ -28,6 +30,7 @@ def commandEnds(request, project, client, execution,
 		task, command, output, ok, info, stats
 		) :
 	s = _server(request)
+	stats = _ast.literal_eval(stats)
 	s.commandEnds(project, client, execution,
 		task, command, output, ok, info, stats
 		)
